73fd47e259090fa0890bb5d2b325e1db6152fd3c
zhangwei217245 <zhangwei217245@a85ea92c-8bd5-11de-a0e8-5982b77fecbe>
2009-08-26 16:29:46 +0000

    SQL提交完毕

---
+++ AuthencationOA/DbDesign/InitializeSQLScripts/6_DocumentType.sql
@@ -0,0 +1,3 @@
+INSERT INTO dbo.documenttype (vc2name) VALUES ('宣传计划书');
+INSERT INTO dbo.documenttype (vc2name) VALUES ('财务审批表');
+INSERT INTO dbo.documenttype (vc2name) VALUES ('网络设备采购申请');
---
+++ AuthencationOA/DbDesign/InitializeSQLScripts/7_DocumentPath.sql
@@ -0,0 +1,8 @@
+INSERT INTO dbo.documentpath (numdoctypeid, numstepindex, tg_id, userid, chverifiertype, vc2stepdesc) VALUES (1, 1, 4, NULL, 'G', '宣传计划书的第1步');
+INSERT INTO dbo.documentpath (numdoctypeid, numstepindex, tg_id, userid, chverifiertype, vc2stepdesc) VALUES (1, 2, 3, NULL, 'G', '宣传计划书的第2步');
+INSERT INTO dbo.documentpath (numdoctypeid, numstepindex, tg_id, userid, chverifiertype, vc2stepdesc) VALUES (1, 3, 2, NULL, 'G', '宣传计划书的第3步');
+INSERT INTO dbo.documentpath (numdoctypeid, numstepindex, tg_id, userid, chverifiertype, vc2stepdesc) VALUES (2, 1, 3, NULL, 'G', '财务审批表的第1步');
+INSERT INTO dbo.documentpath (numdoctypeid, numstepindex, tg_id, userid, chverifiertype, vc2stepdesc) VALUES (2, 2, 2, NULL, 'G', '财务审批表的第2步');
+INSERT INTO dbo.documentpath (numdoctypeid, numstepindex, tg_id, userid, chverifiertype, vc2stepdesc) VALUES (3, 1, 5, NULL, 'G', '网络设备采购申请的第1步');
+INSERT INTO dbo.documentpath (numdoctypeid, numstepindex, tg_id, userid, chverifiertype, vc2stepdesc) VALUES (3, 2, 3, NULL, 'G', '网络设备采购申请的第2步');
+INSERT INTO dbo.documentpath (numdoctypeid, numstepindex, tg_id, userid, chverifiertype, vc2stepdesc) VALUES (3, 3, 2, NULL, 'G', '网络设备采购申请的第3步');
